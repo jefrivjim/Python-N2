26adbf486c9279957c605f5347e4e36c14c0da38
jefrivjim <jeffry94@gmail.com>
2023-07-06 20:52:16 -0600

    add.class

--- Clase_Trig.py
+++ Clase_Trig.py
@@ -1,1 +1,22 @@
-testing
+import math
+
+class Trig ():
+    
+    def __init__(self): #atributo que almacena el valor de Pi
+        self.valorPi = math.pi
+    
+    def seno (self): #metodo para obtener seno de Pi
+        seno = math.sin (self.valorPi)
+        return seno
+
+    def coseno (self): #metodo para obtener coseno de Pi
+        coseno = math.cos (self.valorPi)
+        return coseno
+
+    def tangente (self): #metodo para obtener tangente de Pi
+        tangente = math.tan (self.valorPi)
+        return tangente
+    
+
+
+
