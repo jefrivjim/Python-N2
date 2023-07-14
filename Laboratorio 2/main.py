ef75bb614bce95f36a0e72fd314384951a82215e
jefrivjim <jeffry94@gmail.com>
2023-07-13 21:36:13 -0600

    multiprocessing

--- Laboratorio 2/main.py
+++ Laboratorio 2/main.py
@@ -1,17 +1,63 @@
 from api import *
 from ids import ids
 import time
+import asyncio
+import aiohttp
+import multiprocessing
+import concurrent.futures
+import threading
+
+thread_local = threading.local ()
 
 
-start_time = time.time ()
+def getAllUsers (ids):
+    with multiprocessing.Pool () as pool:
+        diccionarios = pool.map (getOneUser, ids)
+        return diccionarios
+                
+#Sin concurrencia (duracion aproximada 41 segundos)
 
-for id in ids:
-    diccionarios = dict(getOneUser (id))
-    print (diccionarios.get ("name"))
+if __name__== "__main__":
+    start_time = time.time ()
+    diccionario = getAllUsers(ids)
+    for user in diccionario:
+        print (user.get ("name"))
+    duration = time.time () - start_time
+    print (f"La duracion fue: {duration}")  
 
-duration = time.time () - start_time
 
-print (f"La duracion fue: {duration}")
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
 
 
     
