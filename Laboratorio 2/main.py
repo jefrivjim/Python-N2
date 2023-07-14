a2e368f15bf0753c49b7a6239693effbf46fef73
jefrivjim <jeffry94@gmail.com>
2023-07-14 01:02:26 -0600

    codigo_completado

--- Laboratorio 2/main.py
+++ Laboratorio 2/main.py
@@ -1,30 +1,53 @@
 from api import *
 from ids import ids
 import time
-import asyncio
-import aiohttp
 import multiprocessing
 import concurrent.futures
 import threading
 
-thread_local = threading.local ()
+#************* Sin concurrencia **********
 
+""" def getAllUsers (ids):
+    for id in ids: #iteracion entre la lista de ids
+        user = getOneUser(id).get ("name") #variable que guarda el value de "name" de cada diccionario
+        print (user)
+
+if __name__=="__main__":
+    start_time = time.time ()
+    getAllUsers (ids) #llamado de funcion
+    duration = time.time () - start_time
+    print (f"La duracion fue de :{duration}") """
+
+
+#***********Concurrencia usada (multiprocessing) *******************
 
 def getAllUsers (ids):
     with multiprocessing.Pool () as pool:
-        diccionarios = pool.map (getOneUser, ids)
-        return diccionarios
+            diccionarios = pool.map (getOneUser, ids) #genera una lista que corre en procesos diferentes
+            return diccionarios #regresa la lista de diccionarios
                 
-#Concurrencia usada (multiprocessing) duracion aproximada: 6 segundos
-
 if __name__== "__main__":
     start_time = time.time ()
-    diccionario = getAllUsers(ids)
-    for user in diccionario:
-        print (user.get ("name"))
+    users = getAllUsers(ids)
+    for user in users: #iteracion de la lista
+        print (user.get ("name")) #imprime el value de "name" de cada diccionario
     duration = time.time () - start_time
-    print (f"La duracion fue: {duration}")  
+    print (f"La duracion fue: {duration}")
 
+#***********Concurrencia usada (threading)*********************
+
+""" def getAllUsers (ids):
+    with concurrent.futures.ThreadPoolExecutor (max_workers=5) as executor: #genera el pool de threads que corren concurrentemente
+            users = list (executor.map (getOneUser, ids)) 
+            return users #regresa la lista de diccionarios
+
+if __name__=="__main__":
+    start_time = time.time ()
+    users = getAllUsers (ids)
+    for user in users: #iteracion de cada diccionario
+        print (user.get ("name")) #imprime el value de "name" de cada diccionario
+    duration = time.time () - start_time
+    print (f"La duracion fue de: {duration}") """
 
 
 
